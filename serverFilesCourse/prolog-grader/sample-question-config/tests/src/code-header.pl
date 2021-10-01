e43eb1d89d7f6a77639f9ec895a352cd47c6afa5
Steve Wolfman <wolf@mail.ubc.ca>
2021-09-30 23:44:51 -0700

    demonstrating module header

--- serverFilesCourse/prolog-grader/sample-question-config/tests/src/code-header.pl
+++ serverFilesCourse/prolog-grader/sample-question-config/tests/src/code-header.pl
@@ -1,2 +1,5 @@
 % Content in this file is inserted at the top of the student's file.
 % We may decide in future to instead use mustache templating to place student code inside this file.
+
+% It's a good idea to put student code in a module and then limit what it exports.
+:- module(code, [my_reverse/2]).
