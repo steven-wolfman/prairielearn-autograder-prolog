c63a6bb16b618619950acd6b3a47e0965c75c426
Steve Wolfman <wolf@mail.ubc.ca>
2021-09-30 23:33:07 -0700

    renamed image

--- redeploy.sh
+++ redeploy.sh
@@ -3,5 +3,5 @@
 DEFAULT_DOCKER_ID=cswolf
 DOCKER_ID=${1-$DEFAULT_DOCKER_ID}
 
-docker build . -t "${DOCKER_ID}/haskell-prairielearn:latest" && \
-    docker push "${DOCKER_ID}/haskell-prairielearn:latest"
+docker build . -t "${DOCKER_ID}/prairielearn-autograder-prolog:latest" && \
+    docker push "${DOCKER_ID}/prairielearn-autograder-prolog:latest"
