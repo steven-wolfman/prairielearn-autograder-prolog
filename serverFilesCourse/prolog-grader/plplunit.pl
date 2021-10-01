2ca3c01219aaa77f5873dc2b15d6473e7bb6b071
Steve Wolfman <wolf@mail.ubc.ca>
2021-09-30 20:46:10 -0700

    documented

--- serverFilesCourse/prolog-grader/plplunit.pl
+++ serverFilesCourse/prolog-grader/plplunit.pl
@@ -1,7 +1,20 @@
 % The PrairieLearn Prolog Unit Testing library wraps the plunit library.
 %:- module(plplunit, [begin_test_decorated/1, end_test_decorated/1]).
 
-%:- use_module(library(plunit), [begin_tests/2, end_tests/1]).
+% Although it is not defined here, consulting this file does automatically
+% rewrite any test_decorated/3 or test_decorated/4 into a test/2 rule definition
+% suitable for use with plunit.
+%
+% Specifically, treat test_decorated/4 like:
+%
+% test_decorated(+Type: atom, +Name: atom, +Points: integer, ++Options: List)
+%
+% Where Type must be one of required or plain, Points must be greater than 0,
+% and Options must be a legal options list for plunit's test/2.
+%
+% The resulting test produces decorations suitable for the Prolog grader.
+%
+% test_decorated/3 is test_decorated/4 with an empty Options list.
 
 % NOT presently usable with forall option!
 % That would be a great addition.
@@ -28,6 +41,11 @@ type_tag(group, '=G=').
 type_tag(plain, '=P=').
 type_tag(required, '=R=').
 
+%! begin_test_decorated(++Name: Atom)
+%
+% Begin a set of tests with decoration for the Prolog grader.
+% Must be matched with a corresponding end_test_decorated on
+% the same Name.
 begin_test_decorated(Name) :- 
   type_tag(group, GTag), 
   atomic_list_concat([GTag, ' ', Name], FullName),
@@ -37,6 +55,10 @@ begin_test_decorated(Name) :-
                      cleanup((nl, write_term(FooterName, [])))
                     ]).
 
+%! end_test_decorated(++Name: Atom)
+%
+% End a set of tests begun by the corresponding begin_test_decorated
+% on the same Name.
 end_test_decorated(Name) :- 
   end_tests(Name).
 
